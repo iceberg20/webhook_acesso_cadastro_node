f087ef6ded48dc1ccb927bcc1ad5d0088b529a83
iceberg20 <italoberg@yahoo.com.br>
2020-02-27 16:28:11 -0300

    fix string

--- server.js
+++ server.js
@@ -185,6 +185,46 @@ app.post('/situacao', async (req, res) => {
   var nome_mae_converted = convert_name_to_oracle(eleitor_nome_mae);
   var data_nasc_converted = convert_date_to_oracle(eleitor_data_nasc);
 
+  app.post('/getmovie', (req, res) => {
+    const movieToSearch =
+      req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
+        ? req.body.result.parameters.movie
+        : ''
+  
+    const reqUrl = encodeURI(
+      `http://www.omdbapi.com/?t=${movieToSearch}&apikey=${process.env.API_KEY}`
+    )
+    http.get(
+      reqUrl,
+      responseFromAPI => {
+        let completeResponse = ''
+        responseFromAPI.on('data', chunk => {
+          completeResponse += chunk
+        })
+        responseFromAPI.on('end', () => {
+          const movie = JSON.parse(completeResponse)
+  
+          let dataToSend = movieToSearch
+          dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${
+            movie.Director
+          } and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.
+                  }`
+  
+          return res.json({
+            fulfillmentText: dataToSend,
+            source: 'getmovie'
+          })
+        })
+      },
+      error => {
+        return res.json({
+          fulfillmentText: 'Could not get results at this time',
+          source: 'getmovie'
+        })
+      }
+    )
+  })
+
   async function generate_url(){
     let gen = `https://botsociedade.tre-rn.jus.br/api/situacao?nome=${nome_converted}&mae=${nome_mae_converted}&nascimento=${data_nasc_converted}`;
     console.log("#normal urlr:"+gen);
